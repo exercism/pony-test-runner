449b591a837a9b3955408cb7bc66062d34c6e9cd
Red Davies <red@infect.me>
2026-09-23 19:20:13 -0400

    Cleaner implementation of run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -8,10 +8,10 @@ SLUG="$1"
 INPUT_DIR=$(readlink -f $2)
 OUTPUT_DIR=$(readlink -f $3)
 
-rm -f "$OUTPUT_DIR/$SLUG" "$OUTPUT_DIR/compile.*" && mkdir -p "$OUTPUT_DIR"
 cd "$INPUT_DIR"
 
 ponyc . -o "$OUTPUT_DIR" -b "$SLUG" > "$OUTPUT_DIR/compile.stdout" 2> "$OUTPUT_DIR/compile.stderr"
 
 $BASEDIR/bin/pony_test_parser "$SLUG" "$INPUT_DIR" "$OUTPUT_DIR"
+rm -f "$OUTPUT_DIR/$SLUG" "$OUTPUT_DIR/compile.stderr" "$OUTPUT_DIR/compile.stdout"
 
