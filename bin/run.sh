90d7cce68adf8193054208826b851c7583fedce6
Red Davies <red@infect.me>
2026-09-23 18:39:05 -0400

    Applied recommended principles from IsaacG

--- bin/run.sh
+++ bin/run.sh
@@ -3,15 +3,15 @@
 set -u # Exit on undeclared variable
 set -o pipefail # Catch failures in pipes
 
-BASEDIR=`pwd`
+BASEDIR=$PWD
 SLUG="$1"
 INPUT_DIR=$(readlink -f $2)
 OUTPUT_DIR=$(readlink -f $3)
 
-rm -f $OUTPUT_DIR/$SLUG $OUTPUT_DIR/compile.* && mkdir -p $OUTPUT_DIR
-cd $INPUT_DIR
+rm -f "$OUTPUT_DIR/$SLUG" "$OUTPUT_DIR/compile.*" && mkdir -p "$OUTPUT_DIR"
+cd "$INPUT_DIR"
 
-ponyc . -o $OUTPUT_DIR -b $SLUG > $OUTPUT_DIR/compile.stdout 2> $OUTPUT_DIR/compile.stderr
+ponyc . -o "$OUTPUT_DIR" -b "$SLUG" > "$OUTPUT_DIR/compile.stdout" 2> "$OUTPUT_DIR/compile.stderr"
 
-$BASEDIR/bin/pony_test_parser $SLUG $INPUT_DIR $OUTPUT_DIR
+$BASEDIR/bin/pony_test_parser "$SLUG" "$INPUT_DIR" "$OUTPUT_DIR"
 
